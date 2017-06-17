dc93d73e38ae80aff8be80068485a00ab4a2b828
craftwar <coo.ky@hotmail.com>
2017-06-17 09:51:31 +0800

    appveyor: build graphics hook 32bit addintionally

--- CI/before-deploy-win.cmd
+++ CI/before-deploy-win.cmd
@@ -1,3 +1,4 @@
-robocopy C:\projects\obs-studio\build32\rundir\RelWithDebInfo C:\projects\obs-studio\build\ /E /XF .gitignore
+rem robocopy C:\projects\obs-studio\build32\rundir\RelWithDebInfo C:\projects\obs-studio\build\ /E /XF .gitignore
+robocopy C:\projects\obs-studio\build32\rundir\RelWithDebInfo\data\obs-plugins\win-capture C:\projects\obs-studio\build\data\obs-plugins\win-capture /E /XF .gitignore
 robocopy C:\projects\obs-studio\build64\rundir\RelWithDebInfo C:\projects\obs-studio\build\ /E /XC /XN /XO /XF .gitignore
 7z a build.zip C:\projects\obs-studio\build\*
