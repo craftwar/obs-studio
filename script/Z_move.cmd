477be2a9866a5152d9ed3ab1a40dc31a8f551221
craftwar <craftwar@users.noreply.github.com>
2018-08-26 11:18:42 +0800

    CI: Add /GS- to vcpkg compiler flags

---
+++ script/Z_move.cmd
@@ -0,0 +1,14 @@
+cd obs-plugins\64bit
+move /y coreaudio-encoder.* ..\..\Z_backup_obs-plugins\
+move /y enc-amf.* ..\..\Z_backup_obs-plugins\
+move /y frontend-tools.* ..\..\Z_backup_obs-plugins\
+rem move /y obs-filters.*     ..\..\Z_backup_obs-plugins\
+move /y obs-qsv11.*       ..\..\Z_backup_obs-plugins\
+move /y text-freetype2.*  ..\..\Z_backup_obs-plugins\
+move /y vlc-video.*       ..\..\Z_backup_obs-plugins\
+move /y win-decklink.*    ..\..\Z_backup_obs-plugins\
+move /y win-dshow.*       ..\..\Z_backup_obs-plugins\
+move /y win-mf.*          ..\..\Z_backup_obs-plugins\
+move /y obs-vst.*          ..\..\Z_backup_obs-plugins\
+
+
