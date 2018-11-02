c88aa2ee63ea40a78317406dd6f9d3366f067624
craftwar <craftwar@users.noreply.github.com>
2018-11-02 10:17:41 +0800

    CI: Add speexdsp to blend (for mic noise filter?)

--- CI/craftwar/cmake_64bit.cmd
+++ CI/craftwar/cmake_64bit.cmd
@@ -1,11 +1,12 @@
 if "%favor_arch%"=="blend" (
+	vcpkg install speexdsp:x64-windows-static
 :: delete ffmpeg dlls
 ::	del /Q C:\projects\obs-studio\old_dep\win64\bin\av*.*
 ::	del /Q C:\projects\obs-studio\old_dep\win64\bin\swscale-*.*
 ::	del /Q C:\projects\obs-studio\old_dep\win64\bin\swresample-*.*
 ::	7z x -y C:\projects\obs-studio\VCdeps-blend.7z -oC:\projects\obs-studio\old_dep
 ::	set DepsPath64=C:\projects\obs-studio\old_dep\win64
-	cmake -E env CFLAGS="%cl_options%"  CXXFLAGS="%cl_options%" LDFLAGS="-LTCG" cmake -G "Visual Studio 15 2017 Win64" -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_TOOLCHAIN_FILE=C:\Tools\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_EXE_LINKER_FLAGS_INIT=/LTCG -DCMAKE_SHARED_LINKER_FLAGS_INIT=/LTCG -DCMAKE_STATIC_LINKER_FLAGS_INIT=/LTCG -DCMAKE_MODULE_LINKER_FLAGS_INIT=/LTCG -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true -DBUILD_CAPTIONS=true -DCOMPILE_D3D12_HOOK=true -DENABLE_SCRIPTING=false -DBUILD_BROWSER=true -DCEF_ROOT_DIR=%CEF_64% -DCRAFTWAR_MIN_BUILD=false -DDISABLE_SPEEXDSP=true ..
+	cmake -E env CFLAGS="%cl_options%"  CXXFLAGS="%cl_options%" LDFLAGS="-LTCG" cmake -G "Visual Studio 15 2017 Win64" -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_TOOLCHAIN_FILE=C:\Tools\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_EXE_LINKER_FLAGS_INIT=/LTCG -DCMAKE_SHARED_LINKER_FLAGS_INIT=/LTCG -DCMAKE_STATIC_LINKER_FLAGS_INIT=/LTCG -DCMAKE_MODULE_LINKER_FLAGS_INIT=/LTCG -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true -DBUILD_CAPTIONS=true -DCOMPILE_D3D12_HOOK=true -DENABLE_SCRIPTING=false -DBUILD_BROWSER=true -DCEF_ROOT_DIR=%CEF_64% -DCRAFTWAR_MIN_BUILD=false ..
 	"C:\Program Files\Git\usr\bin\sed.exe" -i "/<LanguageStandard>stdcpplatest<\/LanguageStandard>/d" /C/projects/obs-studio/build64/plugins/enc-amf/enc-amf.vcxproj
 ) else (
 	cmake -E env CFLAGS="%cl_options%"  CXXFLAGS="%cl_options%" LDFLAGS="-LTCG" cmake -G "Visual Studio 15 2017 Win64" -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_TOOLCHAIN_FILE=C:\Tools\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_EXE_LINKER_FLAGS_INIT=/LTCG -DCMAKE_SHARED_LINKER_FLAGS_INIT=/LTCG -DCMAKE_STATIC_LINKER_FLAGS_INIT=/LTCG -DCMAKE_MODULE_LINKER_FLAGS_INIT=/LTCG -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true -DBUILD_CAPTIONS=true -DCOMPILE_D3D12_HOOK=false -DENABLE_SCRIPTING=false ..
