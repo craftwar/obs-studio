41e31ca69b569321b15bdaebd4c1489030c4b1b8
craftwar <craftwar@users.noreply.github.com>
2019-02-15 20:51:30 +0800

    obs-ffmpeg: Add comment, b-frames as reference

--- plugins/obs-ffmpeg/obs-ffmpeg-nvenc.c
+++ plugins/obs-ffmpeg/obs-ffmpeg-nvenc.c
@@ -192,6 +192,8 @@ static bool nvenc_update(void *data, obs_data_t *settings)
 
 	av_opt_set(enc->context->priv_data, "level", "auto", 0);
 	av_opt_set_int(enc->context->priv_data, "2pass", twopass, 0);
+	/* b-frames as reference, NV_ENC_BFRAME_REF_MODE_MIDDLE = 0x2 */
+	/* not supported by ffmpeg? */
 	av_opt_set_int(enc->context->priv_data, "b_ref_mode", 2, 0);
 	av_opt_set_int(enc->context->priv_data, "gpu", gpu, 0);
 
