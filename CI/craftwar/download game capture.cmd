ba95826e068e3653ca54136adb01c3b6991fbc78
craftwar <craftwar@users.noreply.github.com>
2020-03-04 22:54:59 +0800

    CI: Create Jim's wc & gc as seperate .7z file
    Download this if user wants hook dlls signed by Jim

--- CI/craftwar/download game capture.cmd
+++ CI/craftwar/download game capture.cmd
@@ -15,6 +15,15 @@ if "%vc_inc_arch%"=="SSE2" (
 	if exist inject-helper32.exe (curl -kLO %cdn_gc_url%/inject-helper32.exe -f --retry 5 -z inject-helper32.exe) else (curl -kLO %cdn_gc_url%/inject-helper32.exe -f --retry 5 -C -)
 	if exist inject-helper64.exe (curl -kLO %cdn_gc_url%/inject-helper64.exe -f --retry 5 -z inject-helper64.exe) else (curl -kLO %cdn_gc_url%/inject-helper64.exe -f --retry 5 -C -)
 	popd
-	xcopy /Y /E Jim_OBS C:\projects\obs-studio\build
+
+	mkdir Jim_OBS\obs-plugins\64bit
+	pushd Jim_OBS\obs-plugins\64bit
+	set file_dir=obs-plugins/64bit
+	set file_url=%cdn_root%/core/%file_dir%
+	if exist win-capture.dll (curl -kLO %file_url%/win-capture.dll -f --retry 5 -z win-capture.dll) else	(curl -kLO %file_url%/win-capture.dll -f --retry 5 -C -)
+	popd
+	
+	7z a gc-%favor_arch%.7z -mx=9 -myx=9 %APPVEYOR_BUILD_FOLDER%\Jim_OBS\*
+::	xcopy /Y /E Jim_OBS C:\projects\obs-studio\build
 )
 endlocal
